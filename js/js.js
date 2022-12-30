9797c118f511bf7aeb255f225e756a315df06cb4
JUAN DAVID RODRIGUEZ RUBIO <jua0729.jr@gmail.com>
2022-12-30 00:48:01 -0500

    JuegoFinalizado

--- js/js.js
+++ js/js.js
@@ -5,6 +5,31 @@ var max = 100;
 var min = 1;
 
 //Numero aleatorio
-var numero_secreto = Math.random();
+var numero_secreto = Math.random() * (max-min) + min;
+
+//Pasar valor a int
+numero_secreto = parseInt(numero_secreto);
 
 console.log(numero_secreto);
+
+var mensaje  = "Ingresa un número para adivinar el numero magico";
+
+while (true) {
+    var numero_usuario = prompt(mensaje,"0");
+
+    numero_usuario = parseInt(numero_usuario);
+
+    if (numero_usuario === numero_secreto) {
+        alert("Ganaste! Adivinaste el número secreto.");
+        break;      
+    }
+    else if (numero_usuario === 0) {
+        break;
+    }
+    else if (numero_usuario > numero_secreto) {
+        mensaje = "Lo sentimos, el número que ingresaste es mayor al número mágico";
+    }
+    else if (numero_usuario < numero_secreto) {
+        mensaje = "Lo sentimos, el número que ingresaste es menor al número mágico";
+    }
+};
